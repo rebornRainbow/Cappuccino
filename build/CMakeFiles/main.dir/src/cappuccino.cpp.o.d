fe0d954fe42dd7e4d2c4686996e531bda917c2a8
1n-hasee <2143572870@qq.com>
2023-03-06 19:47:14 +0800

    遇到一个问题复制构造有基类指的情况

---
+++ build/CMakeFiles/main.dir/src/cappuccino.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/src/cappuccino.cpp.o: \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW5/src/cappuccino.cpp \
+ /usr/include/stdc-predef.h \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW5/include/cappuccino.h
---
+++ build/CMakeFiles/main.dir/src/mocha.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/src/mocha.cpp.o: \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW5/src/mocha.cpp \
+ /usr/include/stdc-predef.h \
+ /home/robin/candy/not_standand/c++/AP1400-2-HW5/include/mocha.h
